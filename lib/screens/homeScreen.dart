264abfdee586a7aa4ec1140ad04495444a4e75e3
salma <salmaelmaghawry91@gmail.com>
2024-04-12 13:36:58 +0200

    creating project

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatelessWidget {
+  const homeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Tune Player '),),
+    
+    );
+  }
+}
