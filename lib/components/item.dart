280a626d5c4f4c6825aaf220afc968eab868bb5f
salma <salmaelmaghawry91@gmail.com>
2024-04-12 15:02:07 +0200

    ui done

--- lib/components/item.dart
+++ lib/components/item.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class Item extends StatelessWidget {
   const Item({super.key , required this.color});
   final Color color;
-
   @override
   Widget build(BuildContext context) {
     return Expanded(
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -3,31 +3,41 @@ import 'package:tune_player_app/components/item.dart';
 
 class homeScreen extends StatelessWidget {
   const homeScreen({super.key});
+  final List<Color> itemsColors = const [
+    Color(0xffEF476F),
+    Color(0xffcdb4db),
+    Color(0xffffc8dd),
+    Color(0xffbde0fe),
+    Color(0xffFFD166),
+    Color(0xffffafcc),
+    Color(0xffa2d2ff),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tune Player '),
-      elevation: 0,
-      backgroundColor: const Color(0xff26313A),
-      centerTitle: true,),
-      body:const Column(
-        children: [
-          Item(color: Color(0xffEF476F),),
-          Item(color: Color(0xff06D6A0),),
-          Item(color: Color(0xff073B4C),),
-          Item(color: Color(0xffFFD166),),
-          Item(color: Color(0xff126782),),
-          Item(color: Color(0xff7371fc),),
-          Item(color: Color(0xff136f63),),
-          
-
-
-
-        ],
+        title: const Text('Tune Player '),
+        elevation: 0,
+        backgroundColor: const Color(0xff26313A),
+        centerTitle: true,
       ),
-    
+      body: Column(
+        children: itemsColors
+            .map(
+              (color) => Item(color: color),
+            )
+            .toList(),
+      ),
     );
+  }
+
+  List<Item> getTuneItem() {
+    List<Item> items = [];
+    for (var color in itemsColors) {
+      items.add(Item(color: color));
+    }
+
+    return items;
   }
 }
