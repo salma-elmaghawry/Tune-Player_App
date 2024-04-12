46116079fc156bb7832e46a29c63b8cbbad82eba
salma <salmaelmaghawry91@gmail.com>
2024-04-12 14:37:41 +0200

    responsive

---
+++ lib/components/item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Item extends StatelessWidget {
+  const Item({super.key , required this.color});
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        
+        color: color,
+      ),
+    );
+  }
+}
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tune_player_app/components/item.dart';
 
 class homeScreen extends StatelessWidget {
   const homeScreen({super.key});
@@ -6,7 +7,26 @@ class homeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tune Player '),),
+      appBar: AppBar(
+        title: Text('Tune Player '),
+      elevation: 0,
+      backgroundColor: const Color(0xff26313A),
+      centerTitle: true,),
+      body:const Column(
+        children: [
+          Item(color: Color(0xffEF476F),),
+          Item(color: Color(0xff06D6A0),),
+          Item(color: Color(0xff073B4C),),
+          Item(color: Color(0xffFFD166),),
+          Item(color: Color(0xff126782),),
+          Item(color: Color(0xff7371fc),),
+          Item(color: Color(0xff136f63),),
+          
+
+
+
+        ],
+      ),
     
     );
   }
